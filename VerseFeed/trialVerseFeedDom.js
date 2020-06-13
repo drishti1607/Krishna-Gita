eb08c65cc5b5eda2f13dde0f1aa8f06185c5250e
drishti1607 <48019207+drishti1607@users.noreply.github.com>
2020-06-13 18:25:12 +0530

    Home page update

--- VerseFeed/trialVerseFeedDom.js
+++ VerseFeed/trialVerseFeedDom.js
@@ -2,22 +2,40 @@ function randomFeedDom(numOfVerses, numOfVersesDisplayed) {
     var verseNum = randomFeed(numOfVerses, numOfVersesDisplayed); //10,3
     var i = 0;
     while(i < numOfVersesDisplayed) {
-        var varVerse = document.getElementById("verse" + (i+1));
+        var varVerse = document.getElementById("verse" + (i+1)); 
         var verseContent = Gita.Verses[(verseNum[i] - 1)].content.shloka;
         varVerse.innerHTML = verseContent;
         var varVerseTrans = document.getElementById("translation" + (i+1));
         var transContent = Gita.Verses[(verseNum[i] - 1)].content.translation;
         varVerseTrans.innerHTML = transContent;
-        var varVerseInsight = document.getElementById("insight" + (i+1));
-        var insightContent = Gita.Verses[(verseNum[i] - 1)].content.insights[0][0]; //for now
-        console.log(insightContent)
-        varVerseInsight.innerHTML = insightContent;
-        var varVerseExp = document.getElementById("explanation" + (i+1));
-        var expContent = Gita.Verses[(verseNum[i] - 1)].content.insights[0][1]; //for now
-        varVerseExp.innerHTML = expContent;
+        var k = 0;
+        while(k < Gita.Verses[(verseNum[i] - 1)].content.insights.length ) {
+            if(Gita.Verses[(verseNum[i] - 1)].content.insights[k][0] != "") {
+                var insightImg= document.createElement("img");
+                insightImg.src="../Assets/Pictures/insight.png";
+                insightImg.setAttribute("class","Img");
+                var insightText=document.createElement("p");
+                insightText.setAttribute("class","subText");
+                insightText.innerHTML=Gita.Verses[(verseNum[i] - 1)].content.insights[k][0];
+                var para = document.getElementById("randomVerse" + (i+1));
+                para.appendChild(insightImg);
+                para.appendChild(insightText);
+                var br = document.createElement("br");
+                para.appendChild(br);
+            }
+            if(Gita.Verses[(verseNum[i] - 1)].content.insights[k][1] != "") {
+                var expImg= document.createElement("img");
+                expImg.src="../Assets/Pictures/explanation.png";
+                expImg.setAttribute("class","Img");
+                var expText=document.createElement("p");
+                expText.setAttribute("class","subText");
+                expText.innerHTML=Gita.Verses[(verseNum[i] - 1)].content.insights[k][1];
+                var para = document.getElementById("randomVerse" + (i+1));
+                para.appendChild(expImg);
+                para.appendChild(expText);
+            }
+            k++;
+        }
         i++;
     }
 }
-
-
-//What is left: take care of case where there is no insight, add for loop for multiple insights and explanations, alignment, addition of picture
